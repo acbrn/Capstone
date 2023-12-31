05d5183f2113aea4d096501780257164e672fffd
Andrew Coburn <coburn188@gmail.com>
2023-12-31 17:36:29 -0600

    changing images

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import html from "html-literal";
-import acbrn from "./assets/img/acbrn.png";
+import acbrn from "assets/img/acbrn.png";
 export default () => html`
   <footer class="footer" id="bottomLine">
     <span><img src="${acbrn}" id="acbrn"/></span>
