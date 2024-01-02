76070f614a0e425c1f3f52d41d182460c5b5fb7f
Andrew Coburn <coburn188@gmail.com>
2024-01-02 13:00:08 -0600

    update code

--- index.js
+++ index.js
@@ -23,21 +23,21 @@ function render(state = store.Home) {
     ${Footer()}
   `;
   router.updatePageLinks();
-  afterRender();
+  // afterRender();
 }
-function afterRender() {
-  const barsIcon = document.querySelector(".fa-bars");
-  const navList = document.querySelector("nav > ul");
+// function afterRender() {
+//   const barsIcon = document.querySelector(".fa-bars");
+//   const navList = document.querySelector("nav > ul");
 
-  // Check if both elements exist before adding the event listener
-  if (barsIcon && navList) {
-    barsIcon.addEventListener("click", () => {
-      navList.classList.toggle("hidden--mobile");
-    });
-  } else {
-    console.error("Bars icon or nav list not found.");
-  }
-}
+//   // Check if both elements exist before adding the event listener
+//   if (barsIcon && navList) {
+//     barsIcon.addEventListener("click", () => {
+//       navList.classList.toggle("hidden--mobile");
+//     });
+//   } else {
+//     console.error("Bars icon or nav list not found.");
+//   }
+// }
 
 router.hooks({
   before: (done, params) => {
